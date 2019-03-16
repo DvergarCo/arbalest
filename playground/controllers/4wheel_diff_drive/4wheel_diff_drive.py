dbffadcf79365e0d711d8b6866782901cdfd0c9c
MagusDei <jami.valorinta@helsinki.fi>
2019-03-16 18:29:20 +0200

    Heavily refactored 4wheel_diff_drive, added keyboard controls

--- playground/controllers/4wheel_diff_drive/4wheel_diff_drive.py
+++ playground/controllers/4wheel_diff_drive/4wheel_diff_drive.py
@@ -4,99 +4,101 @@
 #  from controller import Robot, LED, DistanceSensor
 from controller import Robot
 
-from bluetooth import *
+#from bluetooth import *
 
+def setup_bluetooth():
+    try:
+        server_sock=BluetoothSocket( RFCOMM )
+        server_sock.bind(("", PORT_ANY))
+        server_sock.listen(1)
 
-server_sock=BluetoothSocket( RFCOMM )
-server_sock.bind(("",PORT_ANY))
-server_sock.listen(1)
+        port = server_sock.getsockname()[1]
 
-port = server_sock.getsockname()[1]
+        print("Waiting for connection on RFCOMM channel %d" % port)
 
-print("Waiting for connection on RFCOMM channel %d" % port)
+        cli_sck, client_info = server_sock.accept()
+        print("Accepted connection from ", client_info)
+        cli_sck.setblocking(0)
 
-cli_sck, client_info = server_sock.accept()
-print("Accepted connection from ", client_info)
-cli_sck.setblocking(0)
+        return cli_sck
+    except NameError:
+        # Bluetooth module not loaded.
+        return None
 
-# create the Robot instance.
-
-robot = Robot()
-
-# get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
-
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-#  led = robot.getLED('ledname')
-#  ds = robot.getDistanceSensor('dsname')
-#  ds.enable(timestep)
-
-m_fl = robot.getMotor('front_left')
-m_fr = robot.getMotor('front_right')
-m_rl = robot.getMotor('rear_left')
-m_rr = robot.getMotor('rear_right')
-print(dir(robot))
-print(dir(m_fl))
-
-def setLeft(inp):
-    if inp>0:
-        m_fl.setPosition(float('+inf')) # there must be a better way!
-        m_rl.setPosition(float('+inf'))
-    else:
-        m_fl.setPosition(-float('+inf')) # there must be a better way!
-        m_rl.setPosition(-float('+inf'))
+def setup_motors(robot, motor_names):
+    return [robot.getMotor(motor_name) for motor_name in motor_names]    
     
-    m_fl.setVelocity(inp)
-    m_rl.setVelocity(inp)
-    
-def setRight(inp):
-    if inp>0:
-        m_fr.setPosition(float('+inf')) # there must be a better way!
-        m_rr.setPosition(float('+inf'))
-    else:
-        m_fr.setPosition(-float('+inf')) # there must be a better way!
-        m_rr.setPosition(-float('+inf'))
-    
-    m_fr.setVelocity(inp)
-    m_rr.setVelocity(inp)
+def setMotors(inp, motors):
+    mult = 1 if inp > 0 else -1
+    for motor in motors:
+        motor.setPosition(mult*float('+inf'))
+        motor.setVelocity(inp)
 
-
-def remoteFunc():
+def remoteFunc(cli_sck, l_motors, r_motors):
     try:
         data = cli_sck.recv(1024)
         if len(data) == 0: pass
-#        print("received [%s]" % data)
+#       print("received [%s]" % data)
         if data[0] == 'w':
-            setRight(10)
+            setMotors(10, r_motors)
         if data[0] == 's':
-            setRight(-10)
+            setMotors(-10, r_motors)
         if data[0] == '0':
-            setRight(0)
+            setMotors(0, r_motors)
             
         if data[0] == 'u':
-            setLeft(10)
+            setMotors(10, l_motors)
         if data[0] == 'j':
-            setLeft(-10)
+            setMotors(-10, l_motors)
         if data[0] == 'o':
-            setLeft(0)
-            
-        
+            setMotors(0, l_motors)
     except IOError:
         pass
 
+def keyboard_controls(keyboard, motors, keys_speeds):
+    """Keyboard controls for the robot.
+    
+    Params:
+    keyboard (Keyboard) -- A Webots keyboard instance.
+    motors (tuple) -- tuple containing all available motors.
+    keys_speeds (dict) -- keyboard_key (str) -> (speed per motor).
+    """
+    current_key = keyboard.getKey()
+    if current_key == -1:
+        setMotors(0, motors)
+        return
+    for kb_key, speeds in keys_speeds.items():
+        if (current_key == ord(kb_key)):
+            for i, speed in enumerate(speeds):
+                setMotors(speed, [motors[i]])
+            break
 
-# Main loop:
-# - perform simulation steps until Webots is stopping the controller
-while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
+if __name__ == "__main__":
+    cli_sck = setup_bluetooth()
+    
+    robot = Robot()
+    timestep = int(robot.getBasicTimeStep())
+    keyboard = robot.getKeyboard()
+    keyboard.enable(timestep)
+    (m_fl, m_fr, m_rl, m_rr) = setup_motors(robot, ["front_left", "front_right", "rear_left", "rear_right"])
+    
+    # Main loop:
+    # - perform simulation steps until Webots is stopping the controller
+    while robot.step(timestep) != -1:
+        # Read the sensors:
+        # Enter here functions to read sensor data, like:
+        #  val = ds.getValue()
 
-    # Process sensor data here.
-    remoteFunc()
-    # Enter here functions to send actuator commands, like:
-    #  led.set(1)
-    pass
+        # Process sensor data here.
 
-# Enter here exit cleanup code.
+        #remoteFunc(cli_sck, (m_fl, m_rl), (m_fr, m_rr))
+        keyboard_controls(keyboard, (m_fl, m_rl, m_fr, m_rr),
+                          {"W": (10, 10, 10, 10),
+                           "S": (-10, -10, -10, -10), 
+                           "A": (10, 10, -10, -10),
+                           "D": (-10, -10, 10, 10)})
+
+        # Enter here functions to send actuator commands, like:
+        #  led.set(1)
+    print("Closing...")
+    # Enter here exit cleanup code.
