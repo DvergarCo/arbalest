999f131f59d7e6c726a54421e3412e79cecb7cfe
semih <semih.iseri@gmail.com>
2019-03-16 12:24:14 +0200

    bluetooth controllable

--- playground/controllers/4wheel_diff_drive/4wheel_diff_drive.py
+++ playground/controllers/4wheel_diff_drive/4wheel_diff_drive.py
@@ -4,7 +4,23 @@
 #  from controller import Robot, LED, DistanceSensor
 from controller import Robot
 
+from bluetooth import *
+
+
+server_sock=BluetoothSocket( RFCOMM )
+server_sock.bind(("",PORT_ANY))
+server_sock.listen(1)
+
+port = server_sock.getsockname()[1]
+
+print("Waiting for connection on RFCOMM channel %d" % port)
+
+cli_sck, client_info = server_sock.accept()
+print("Accepted connection from ", client_info)
+cli_sck.setblocking(0)
+
 # create the Robot instance.
+
 robot = Robot()
 
 # get the time step of the current world.
@@ -23,15 +39,52 @@ m_rr = robot.getMotor('rear_right')
 print(dir(robot))
 print(dir(m_fl))
 
-m_fl.setPosition(9999999) # there must be a better way!
-m_rl.setPosition(9999999)
-m_fl.setVelocity(10)
-m_rl.setVelocity(10)
+def setLeft(inp):
+    if inp>0:
+        m_fl.setPosition(float('+inf')) # there must be a better way!
+        m_rl.setPosition(float('+inf'))
+    else:
+        m_fl.setPosition(-float('+inf')) # there must be a better way!
+        m_rl.setPosition(-float('+inf'))
+    
+    m_fl.setVelocity(inp)
+    m_rl.setVelocity(inp)
+    
+def setRight(inp):
+    if inp>0:
+        m_fr.setPosition(float('+inf')) # there must be a better way!
+        m_rr.setPosition(float('+inf'))
+    else:
+        m_fr.setPosition(-float('+inf')) # there must be a better way!
+        m_rr.setPosition(-float('+inf'))
+    
+    m_fr.setVelocity(inp)
+    m_rr.setVelocity(inp)
 
-m_fr.setPosition(9999999)
-m_rr.setPosition(9999999)
-m_fr.setVelocity(5)
-m_rr.setVelocity(5)
+
+def remoteFunc():
+    try:
+        data = cli_sck.recv(1024)
+        if len(data) == 0: pass
+#        print("received [%s]" % data)
+        if data[0] == 'w':
+            setRight(10)
+        if data[0] == 's':
+            setRight(-10)
+        if data[0] == '0':
+            setRight(0)
+            
+        if data[0] == 'u':
+            setLeft(10)
+        if data[0] == 'j':
+            setLeft(-10)
+        if data[0] == 'o':
+            setLeft(0)
+            
+        
+    except IOError:
+        pass
+
 
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
@@ -41,7 +94,7 @@ while robot.step(timestep) != -1:
     #  val = ds.getValue()
 
     # Process sensor data here.
-
+    remoteFunc()
     # Enter here functions to send actuator commands, like:
     #  led.set(1)
     pass
